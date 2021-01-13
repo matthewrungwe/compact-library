b230ddaa08cc2273705b2f72e865658b0ea25a7a
Matthew Rungwe <matthewrungwe@gmail.com>
2021-01-13 14:16:22 +0200

    Add comments to area.js file.

--- js/area.js
+++ js/area.js
@@ -1,24 +1,35 @@
+/* 
+    Written by
+    Matthew Rungwe
+*/
 
+// Calculating area of a square function.
 const square = (side) => {
     const calculation = side * side;
     return calculation;
 };
 
+// Calculating the area of a triangle fuction.
 const triangle = (base, height) => {
     const calculation = 1/2 * base * height;
     return calculation;
 };
 
+// Calculating the area of a rectangle function.
 const rectangle = (length, width) => {
     const calculation = length * width;
     return calculation;
 };
 
+// Calculating the area of a trapezium function.
 const trapezium = (lengthA, lengthB, height) => {
     const calculation = 2 * (lengthA + lengthB) * height;
     return calculation;
 };
 
+// the area function deligates which shape's function gets called.
+// area() takes at most 4 arguments when it gets called
+// area() needs to have the proper name of the shape you wish to calculate for as a string in the first argument 
 const area = (shape, sideA = 0, sideB = 0, sideC = 0) => {
     switch (shape) {
         case 'triangle':
@@ -33,5 +44,4 @@ const area = (shape, sideA = 0, sideB = 0, sideC = 0) => {
             console.log('unknown shape entered');
             break;
     }
-
 }; 
