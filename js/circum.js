327993b6801be1bf4698e8d819bb2e31f55f570e
Matthew Rungwe <matthewrungwe@gmail.com>
2021-01-19 00:00:15 +0200

    Update comments in circum.js file.

--- js/circum.js
+++ js/circum.js
@@ -1,4 +1,6 @@
-// Function to calculate circumference of a circle.
+// Calculating circumference of a circle.
+// function accepts one argument, the radius of the circle.
+// function returns the circumference as a number
 const circum = (radius) => {
     const pi = 3.142;
 
