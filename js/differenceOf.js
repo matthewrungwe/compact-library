12d55729160e8d93e565b27408ec7cc6f0b06fdd
Matthew Rungwe <matthewrungwe@gmail.com>
2021-01-19 11:52:37 +0200

    Add comments to differenceOf.js file.

--- js/differenceOf.js
+++ js/differenceOf.js
@@ -1,7 +1,13 @@
-const differenceOf = newFunction()
+// Function to calculate the difference between two number.
 
-const newFunction = () => {
+// wrapFunction returns a function.
+const wrapFunction = () => {
     return (a, b) => {
         return a - b;
     };
-}
+};
+
+// Calling wrapFunction returns a function definition.
+// The fuction returned by wrapFunction has two parameter
+// To use differenceOf, call it with two arguments to return the difference between them
+const differenceOf = wrapFunction();
